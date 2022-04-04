3beb6c9f7599b3131cc195008213937b9017e988
dhruvbansal17661 <43857178+dhruvbansal17661@users.noreply.github.com>
2022-04-04 12:24:51 +0530

    Update HelloWorldServlet.java

--- src/main/java/com/codetutr/HelloWorldServlet.java
+++ src/main/java/com/codetutr/HelloWorldServlet.java
@@ -14,6 +14,6 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
             throws ServletException, IOException {
-        resp.getOutputStream().write("Hello World.".getBytes());
+        resp.getOutputStream().write("Hello World from new_branch1.".getBytes());
     }
 }
